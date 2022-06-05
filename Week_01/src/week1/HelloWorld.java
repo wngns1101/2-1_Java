69d4602a45d54d568c54df9f0b2d25f5de9918ad
이주훈 <86885227+wngns1101@users.noreply.github.com>
2022-06-05 21:41:58 +0900

    1주차

---
+++ Week_01/src/week1/HelloWorld.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("학과: 컴퓨터정보과");
+		System.out.println("학번: 201944096");
+		System.out.println("이름: 이주훈");
+	}
+}
